e576db881b4affacd3ef8065e84862d14c935877
Daiane Schmitz <rpl.daia@gmail.com>
2020-03-07 16:19:40 -0300

    calculadora e comparador

--- src/App.java
+++ src/App.java
@@ -2,7 +2,22 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        
+        Calculadora calculadora = new Calculadora();
+        int somaIdades = calculadora.somar(40, 20);
+        System.out.println("A soma das idades é: " + somaIdades);
+
+        int subtraiIdades = calculadora.subtrair(40, 20);
+        System.out.println("A subtração das idades é: " + subtraiIdades);
+
+        int divideIdades = calculadora.dividir(40, 20);
+        System.out.println("A divisão das idades é: " + divideIdades);
+
+        int multiplicaIdades = calculadora.multiplicar(40, 20);
+        System.out.println("A multiplicação das idades é: " + multiplicaIdades);
+
+        Comparador comparador = new Comparador();
+        boolean comparando = comparador.menorQue(1, 5);
+
+        System.out.println("O primeiro valor é menor que o segundo? " + comparando);
     }
 }
